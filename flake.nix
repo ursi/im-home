b3edd1a7c3b3b436dd76fbdbdea759b978fac629
Mason Mackaman <masondeanm@aol.com>
2021-07-03 20:10:56 -0400

    Add i3status module

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
     { nixosModules =
         { git = ./git.nix;
           i3 = ./i3.nix;
+          i3status = ./i3status.nix;
           icons = ./icons.nix;
           links = ./links.nix;
         };
---
+++ i3status.nix
@@ -0,0 +1,84 @@
+with builtins;
+{ pkgs, lib, ...}:
+  let
+    l = lib; p = pkgs; t = l.types;
+    augment = import ./augment.nix l;
+  in
+  { imports = [ ./links.nix ];
+
+    options =
+      augment
+        ({ config, ... }:
+           { options =
+               { i3status =
+                   l.mkOption
+                     { type =
+                         t.nullOr
+                           (t.submodule
+                              { options =
+                                  { output-format =
+                                      l.mkOption
+                                        { type = t.str;
+                                          default = "i3bar";
+                                        };
+
+                                    status-bar =
+                                      l.mkOption
+                                        { type =
+                                            t.listOf
+                                              (t.submodule
+                                                 { options =
+                                                     { name = l.mkOption { type = t.str; };
+
+                                                       config =
+                                                         l.mkOption
+                                                           { type = t.attrsOf t.str; };
+                                                     };
+                                                 }
+                                              );
+
+                                          default = [];
+                                        };
+                                  };
+                              }
+                           );
+
+                       default = null;
+                     };
+               };
+
+             config.links =
+               let cfg = config.i3status; in
+               l.mkIf (cfg != null)
+                 { lines."/.config/i3status/config" =
+                     ''
+                     general {
+                         output_format = "${cfg.output-format}"
+                     }
+
+                     ${concatStringsSep "\n"
+                         (map (module: ''order += "${module.name}"'') cfg.status-bar)
+                     }
+
+                     ${concatStringsSep "\n"
+                         (map
+                            (module:
+                               ''
+                               ${module.name} {
+                                 ${concatStringsSep "\n"
+                                     (l.mapAttrsToList
+                                        (n: v: ''${n} = "${v}"'')
+                                        module.config
+                                     )
+                                 }
+                               }
+                               ''
+                            )
+                            cfg.status-bar
+                         )
+                     }
+                     '';
+                 };
+           }
+        );
+  }
